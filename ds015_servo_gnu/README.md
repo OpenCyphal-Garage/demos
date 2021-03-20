fa2254188265a68d057bbf4660c3bc2005026f22
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-03-20 03:27:03 +0200

    Integrate Nunavut

--- ds015_servo_gnu/README.md
+++ ds015_servo_gnu/README.md
@@ -1,1 +1,3 @@
- 
+# DS-015 servo demo
+
+
--- ds015_servo_gnu/src/main.c
+++ ds015_servo_gnu/src/main.c
@@ -1,1 +1,15 @@
- 
+// This software is distributed under the terms of the MIT License.
+// Copyright (C) 2021 UAVCAN Consortium <consortium@uavcan.org>
+// Author: Pavel Kirienko <pavel@uavcan.org>
+
+#include <stdio.h>
+#include <canard.h>
+#include "socketcan.h"
+#include <uavcan/node/Heartbeat_1_0.h>
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
