e84ecb885869eb09caaf0464a7e519c53139e5fd
noxuz <rguez.abraham@gmail.com>
2021-03-31 19:38:07 -0700

    fixed startup_s32k146.S no appearing in generated makefile and compiler errors in app.c, canfd.c

--- ds015_servo/platform/s32k146/include/LPIT.h
+++ ds015_servo/platform/s32k146/include/LPIT.h
@@ -21,6 +21,8 @@ uint64_t LPIT0_GetTimestamp(void);
 
 void LPIT0_Ch2_IRQ_Config(uint32_t interrupt_freq, uint8_t interrupt_priority, void (*callback)());
 
+extern void LPIT0_Ch2_IRQHandler(void);
+
 #ifdef __cplusplus
 }
 #endif
